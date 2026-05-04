c842abb5496dccb079a5d23ae5f5acb3cdee774f
vashist-vineet <vineetvashist17@gmail.com>
2026-05-04 23:43:52 +0530

    updates resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Vineet_Vashist_Product_Manager_Resume.pdf";
+import pdf from "../../Assets/VINEET_PM_RESUME.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
